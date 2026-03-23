ba5dcdbd9ad1ad18a8b2a7af1b00e044a277cce8
Sydney Eckstein <sydney.eckstein@fsinvestments.com>
2026-03-23 15:20:56 +0000

    Update model.py with decision tree classifier for Titanic survival prediction

--- Titanic/model.py
+++ Titanic/model.py
@@ -11,10 +11,32 @@ from sklearn.metrics import accuracy_score
 
 
 # Open the Titanic dataset
-df = pd.read_csv('Titanic.csv')
+df = pd.read_csv('Titanic (1).csv')
 
 
 # Print the first few lines of the dataframe
 #
 # Check the names and types of each column
 print(df.head())
+
+# scikit-learn can't work with arbitrary strings, like "male" and "female"
+# Create a new field that turns the "Sex" string into a binary number
+df['Sex_numeric'] = np.where(df['Sex'] == 'male', 1, 0)
+
+
+# Pull out the Pclass, Age, and Sex_numeric columns into their own dataframe
+X = df[['Pclass', 'Age', 'Sex_numeric']]
+
+
+# Extract the target variable
+y = df.pop('Survived').values
+print(y)
+
+# Create the Decision TreeClassifier object
+# max_depth=2 restricts the size of the tree to only two splits
+clf = tree.DecisionTreeClassifier(max_depth=2)
+
+
+# Fit the decision tree
+# Our goal is to predict Survival based on Pclass, Age, and Sex
+clf.fit(X, y) 
